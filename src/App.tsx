2ec5f1a8325e0885892f501b93223077d82beddb
KenielDev <103508290+KenielDev@users.noreply.github.com>
2022-10-13 16:39:25 -0300

    WIP: Let´s Coding!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import { useState } from 'react'
+import { Component, useEffect, useState } from "react";
+import { render } from "react-dom";
 
 function App() {
-  return <div>a</div>
+    const [word, setWord] = useState("");
+
+    function getSignificados(palavra: any) {
+        fetch(`https://significado.herokuapp.com/v2/${palavra}`)
+            .then((response) => response.json())
+            .then((result) => {})
+            .catch((error) => console.log("error", error));
+    }
+
+    return (
+        <div>
+            <p>Significados</p>
+            <input
+                type="text"
+                placeholder="buscar"
+                onChange={(e) => {
+                    setWord(e.target.value);
+                }}
+            />
+            <button>dale</button>
+            <p>palavra: {word}</p>
+        </div>
+    );
 }
 
-export default App
+export default App;
